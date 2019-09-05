8fafc3a1f8786be97e76991ba23e251c952bcc1b
pbabic <pbabic@gmail.com>
2019-09-05 21:25:06 +0200

    novi tooltip

--- ImageOrganizer/Program.cs
+++ ImageOrganizer/Program.cs
@@ -17,9 +17,16 @@ namespace ImageOrganizer
         static void Main()
         {
 
-            if (MessageBox.Show("This application is provided AS IS. Great effort is invested searching for any application malfunctioning and fixing them.\n\n" +
-                            "I do not accept any responsibility for eventual errors or loss of files that could happen using this application.\n\nClicking Ok you are agreeing to these terms.\n\n", 
-                            "DISCLAIMER", MessageBoxButtons.OKCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) != DialogResult.OK) return;
+            if (!((bool)Properties.Settings.Default["agreement"]) && MessageBox.Show("This application is provided AS IS. Great effort is invested searching for any application malfunctioning and fixing them.\n\n" +
+                            "I do not accept any responsibility for eventual errors or loss of files that could happen using this application.\n\nClicking Ok you are agreeing to these terms.\n\n",
+                            "DISCLAIMER", MessageBoxButtons.OKCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) != DialogResult.OK)
+            {
+                return;
+            }
+            
+            Properties.Settings.Default["agreement"] = true;
+            Properties.Settings.Default.Save();
+
             Logger.Setup();
             AppDomain.CurrentDomain.FirstChanceException += FirstChanceHandler;
             Application.EnableVisualStyles();
